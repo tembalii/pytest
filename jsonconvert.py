3b25ae1c6bd39de65836bffc7d24b7aa041ba71a
tembalii <michael.bali@semgrep.com>
2024-03-07 20:47:25 +0000

    testing files"

--- jsonconvert.py
+++ jsonconvert.py
@@ -6,7 +6,6 @@ import os
 from urllib.parse import quote
 
 
-
 def get_deployments():
     headers = {"Accept": "application/json", "Authorization": "Bearer " + SEMGREP_APP_TOKEN}
 
@@ -32,6 +31,33 @@ def get_projects(slug_name):
         get_findings_per_project(slug_name, project_name)
 
 
+def get_github_url(repo_url, ref, file_path, line_number):
+    # Extract branch name from ref
+    if ref.startswith("refs/heads/"):
+        branch_name = ref.split("refs/heads/")[-1]
+    elif ref.startswith("refs/pull/"):
+        branch_name = "pull/" + ref.split("refs/pull/")[-1].split("/")[0]
+    else:
+        branch_name = ref
+
+    # Handle "master" branch
+    if branch_name == "master":
+        encoded_branch = "master"
+    # Handle "main" branch
+    elif branch_name == "main":
+        encoded_branch = "main"
+    else:
+        # For other branches, use the branch name as is
+        encoded_branch = quote(branch_name)
+
+    # Encode file path for URL
+    encoded_file_path = quote(file_path)
+
+    # Construct GitHub URL
+    github_url = f"{repo_url}/blob/{encoded_branch}/{encoded_file_path}#L{line_number}"
+    return github_url
+
+
 def get_findings_per_project(slug_name, project_name):
     headers = {"Accept": "application/json", "Authorization": "Bearer " + SEMGREP_APP_TOKEN}
 
@@ -47,20 +73,15 @@ def get_findings_per_project(slug_name, project_name):
         repo_url = finding['repository']['url']
         file_path = finding['location']['file_path']
         line_number = finding['location']['line']
-        # github_url = f"{repo_url}/blob/master/{file_path}#L{line_number}"
 
-        # Use ref to determine branch or fallback to "master" if ref is not provided
-        encoded_file_path = quote(file_path)
-        encoded_branch = quote(ref) if ref else "master"
-        
-        github_url = f"{repo_url}/blob/{encoded_branch}/{encoded_file_path}#L{line_number}"
+        # Construct GitHub URL based on ref
+        github_url = get_github_url(repo_url, ref, file_path, line_number)
         finding['github_url'] = github_url
 
     # Write modified JSON data to a file
     file_path = re.sub(r"[^\w\s]", "", project_name) + ".json"
     with open(file_path, "w") as file:
         json.dump(data, file)
-
 
 
 if __name__ == "__main__":
